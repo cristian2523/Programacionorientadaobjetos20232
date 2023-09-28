21ba1f78f3d556cb4242629fcbf31661d63f1b68
cristian2523 <cristhian787-@hotmail.com>
2023-09-27 20:59:41 -0500

    add sumatoria de notas

--- src/edu/ucaldas/ingenieria/poo/gradebook/GradeBookTest.java
+++ src/edu/ucaldas/ingenieria/poo/gradebook/GradeBookTest.java
@@ -13,15 +13,16 @@ public class GradeBookTest {
 			Gradebook gradebook = new Gradebook();
 			Gradebook gradebookProgramacionOO = new Gradebook("Programacion Orientada a Objetos ");
 			
+			gradebook.determimeClassAveriguar();
 			
-			
+			/*
 			System.out.print("give a course name: ");
 			String courseName = sc.nextLine();
 			gradebook.setCourseName(courseName);
 			System.out.println(" nombre del curso "+ gradebook.getCourseName());
 			System.out.println("Nombre del curso con constructor" + gradebook);
 			gradebook.displayMessage(courseName);
-			
+			*/
 		}
 
 	}
--- src/edu/ucaldas/ingenieria/poo/gradebook/Gradebook.java
+++ src/edu/ucaldas/ingenieria/poo/gradebook/Gradebook.java
@@ -1,5 +1,7 @@
 package edu.ucaldas.ingenieria.poo.gradebook;
 
+import java.util.Scanner;
+
 public class Gradebook {
 	
 	private String courseName;
@@ -30,4 +32,22 @@ public class Gradebook {
 		System.out.print(" hello from GadeBook class " + getCourseName());
 	 }
 
+	public void determimeClassAveriguar() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int total = 0, grade, gradeCounter = 1, average;
+		
+		while (gradeCounter <= 5) {
+			System.out.println(" Por favor ingresar grado");
+			grade = sc.nextInt();
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+			
+		}
+		
+		average = total/5;
+		System.out.println("El promedio de las notas son "+ average);
+			
+	}
 }
